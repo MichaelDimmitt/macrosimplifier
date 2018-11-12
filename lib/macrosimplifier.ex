c9117d72feceeae1421add40a104f90f255a6b57
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-11-12 09:30:40 -0500

    inspect the pieces of a macro 🧐

--- lib/macrosimplifier.ex
+++ lib/macrosimplifier.ex
@@ -3,4 +3,18 @@ defmodule Macrosimplifier do
   def hello do
     :world
   end
+  # {:*, [line: 50], [3, 3]}
+  # {:+, [line: 50], [{:*, [line: 50], [3, 2]}, 1]}
+
+  defmacro interference(a_str, macro, {op1, _, [valueOne | valueRest]}) do
+    IO.inspect binding()
+  end
+
+end
+
+defmodule Calculationmachine do
+  def go do
+    require Macrosimplifier
+    Macrosimplifier.interference('2+1', 2+1, 2+1)
+  end
 end

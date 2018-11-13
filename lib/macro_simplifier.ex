52ec57745b250b921c400313ed2d43783b2a2828
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-11-12 19:45:29 -0500

    Better function and module naming

--- lib/macro_simplifier.ex
+++ lib/macro_simplifier.ex
@@ -1,26 +1,26 @@
 defmodule MacroSimplifier do
-  def hello do
-    :world
-  end
   # {:*, [line: 50], [3, 3]}
   # {:+, [line: 50], [{:*, [line: 50], [3, 2]}, 1]}
 
   defmacro interference(macro = {op1, _, [valueOne | valueRest]}) do
-    [ calc: Macro.to_string(macro),
-      macro: macro |> Tuple.to_list,
+    [
+      calc: Macro.to_string(macro),
+      macro: macro |> Tuple.to_list(),
       simp: [op1, valueOne, valueRest],
       op1: op1,
       values: [valueOne] ++ valueRest,
       result: macro
     ]
   end
-
 end
 
 # lib/calculation_machine.ex
-defmodule CalculationMachine do
-  def go do
+defmodule AbacusClerk do
+  def calculate do
     require MacroSimplifier
-    MacroSimplifier.interference(2 + 1)
+
+    #    MacroSimplifier.interference(2 + 1 * 2 + 1)
+    MacroSimplifier.interference(2 + 2 * 3)
+    #    MacroSimplifier.info(2+1)
   end
 end
--- lib/mix/tasks/runn.ex
+++ lib/mix/tasks/runn.ex
@@ -4,10 +4,7 @@ defmodule Mix.Tasks.Runn do
   @shortdoc "Simply runs the Hello.say/0 function"
   def run(_) do
     # calling our Hello.say() function from earlier
-    require MacroSimplifier
-    MacroSimplifier.interference(2 + 1)
-
-    CalculationMachine.go()
+    AbacusClerk.calculate
     |> IO.inspect()
   end
 end

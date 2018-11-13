bd8f36c8658ecd0c1e51351c1a93c843d1c8987f
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-11-12 20:27:34 -0500

    add function to simplify the entire macro

--- lib/macro_simplifier.ex
+++ lib/macro_simplifier.ex
@@ -1,26 +1,54 @@
 defmodule MacroSimplifier do
+
   # {:*, [line: 50], [3, 3]}
   # {:+, [line: 50], [{:*, [line: 50], [3, 2]}, 1]}
 
+  ## goal
+  ## defmacro interference(macro = {op1, _, [valueOne | valueRest]}) do
+  ##   [ calc: Macro.to_string(macro),
+  ##     macro: macro |> Tuple.to_list,
+  ##     simp: [op1, valueOne, valueRest],
+  ##     op1: op1,
+  ##     values: [valueOne] ++ valueRest,
+  ##     result: macro
+  ##   ]
+
+  def simplify_entire({opNext, _, [head | tail]}) do
+    [opNext, [simplify_entire(head), simplify_entire(tail)]]
+  end
+
+  def simplify_entire([atomOrTuple]) do
+    simplify_entire(atomOrTuple)
+  end
+
+  def simplify_entire(atom) do
+    atom
+  end
+
+  def simplify(macro = {op1, _, list = [valueOne | valueRest]}) do
+    [op1, list, valueOne, valueRest]
+    [entire_simp: simplify_entire(macro)]
+  end
+
+
   defmacro interference(macro = {op1, _, [valueOne | valueRest]}) do
-    [
-      calc: Macro.to_string(macro),
-      macro: macro |> Tuple.to_list(),
-      simp: [op1, valueOne, valueRest],
-      op1: op1,
-      values: [valueOne] ++ valueRest,
-      result: macro
-    ]
+    [op1, valueOne, valueRest]
+    # macro  |> exampleStillHaveTheQuotedValue()
+    # |> IO.inspect
+
+    macro
+    |> simplify
   end
 end
 
 # lib/calculation_machine.ex
 defmodule AbacusClerk do
+
   def calculate do
     require MacroSimplifier
 
     #    MacroSimplifier.interference(2 + 1 * 2 + 1)
-    MacroSimplifier.interference(2 + 2 * 3)
+    MacroSimplifier.interference((2) + 2 * 3)
     #    MacroSimplifier.info(2+1)
   end
 end
--- test/macro_simplifier_test.exs
+++ test/macro_simplifier_test.exs
@@ -3,15 +3,16 @@ defmodule MacroSimplifierTest do
 
   test "simplify 2 + 1 ast" do
     require MacroSimplifier
-    assert MacroSimplifier.interference(2 + 1) === [
-      calc: "2 + 1",
-      # line is the current line number from the MacroSimplifier. Excluding comments.
-      macro: [:+, [line: 6], [2, 1]],
-      simp: [:+, 2, [1]],
-      op1: :+,
-      values: [2, 1],
-      result: 3
-    ]
+    ## assert MacroSimplifier.interference(2 + 1) === [
+    ##   calc: "2 + 1",
+    ##   # line is the current line number from the MacroSimplifier. Excluding comments.
+    ##   macro: [:+, [line: 6], [2, 1]],
+    ##   simp: [:+, 2, [1]],
+    ##   op1: :+,
+    ##   values: [2, 1],
+    ##   result: 3
+    ## ]
+
+    assert MacroSimplifier.interference(2 + 2 * 3) === [entire_simp: [:+, [2, [:*, [2, 3] ] ] ]]
   end
 end
-

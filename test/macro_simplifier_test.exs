7f74e970537ffcdce2e91953d4e2ee8f3944b57d
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-11-12 22:01:20 -0500

    set expectations for test simplified_num_values function to pass.

--- test/macro_simplifier_test.exs
+++ test/macro_simplifier_test.exs
@@ -5,35 +5,40 @@ defmodule MacroSimplifierTest do
   test "simplify ast: '2 + 2 * 3' " do
     assert MacroSimplifier.interference(2 + 2 * 3) === [
       simplified_full_macro: [:+, [2, [:*, [2, 3]]]],
-      simplified_operations: [:+, :*]
+      simplified_operations: [:+, :*],
+      simplified_num_values: [2, 2, 3]
     ]
   end
 
   test "simplify ast: '3 * (2 + 1)' " do
     assert MacroSimplifier.interference(3 * (2 + 1)) === [
       simplified_full_macro: [:*, [3, [:+, [2, 1]]]],
-      simplified_operations: [:*, :+]
+      simplified_operations: [:*, :+],
+      simplified_num_values: [3, 2, 1]
     ]
   end
 
   test "simplify ast: '(3 * 2) + 1' " do
     assert MacroSimplifier.interference((3*2)+1) === [
       simplified_full_macro: [:+, [[:*, [3, 2]], 1]],
-      simplified_operations: [:+, :*]
+      simplified_operations: [:+, :*],
+      simplified_num_values: [3, 2, 1]
     ]
   end
 
   test "simplify ast: '3 * 2 + 1' )" do
     assert MacroSimplifier.interference(3*2+1) === [
       simplified_full_macro: [:+, [[:*, [3, 2]], 1]],
-      simplified_operations: [:+, :*]
+      simplified_operations: [:+, :*],
+      simplified_num_values: [3, 2, 1]
     ]
   end
 
   test "simplify ast: '3 * 2 + 1 * 7 + 2 - 1 * 2' " do
     assert MacroSimplifier.interference(3 * 2 + 1 * 7 + 2 - 1 * 2) === [
       simplified_full_macro: [:-, [[:+, [[:+, [[:*, [3, 2]], [:*, [1, 7]]]], 2]], [:*, [1, 2]]]],
-      simplified_operations: [:-, :+, :+, :*, :*, :*]
+      simplified_operations: [:-, :+, :+, :*, :*, :*],
+      simplified_num_values: [3, 2, 1, 7, 2, 1, 2]
     ]
 
   end
